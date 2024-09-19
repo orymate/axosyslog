cdef38602dfe0bd85f577cecacb07d97558e3faf
Szilard Parrag <szilard.parrag@axoflow.com>
2024-09-19 12:27:38 +0200

    lib/filterx: add E2E test for startswith/endswith/includes
    Signed-off-by: Szilard Parrag <szilard.parrag@axoflow.com>

--- tests/light/functional_tests/filterx/test_filterx.py
+++ tests/light/functional_tests/filterx/test_filterx.py
@@ -2276,3 +2276,31 @@ def test_parse_windows_eventlog_xml(config, syslog_ng):
             },
         },
     }
+
+
+def test_startswith_endswith_includes(config, syslog_ng):
+    (file_true, file_false) = create_config(
+        config, r"""
+    result = json();
+    if (startswith($MSG, ["dummy_prefix", "foo"]))
+    {
+        result.startswith_foo = true;
+    };
+    bar_var = "bar";
+    if (includes($MSG, bar_var, ignorecase=true))
+    {
+        result.contains_bar = true;
+    };
+    baz_list = ["baz"];
+    if (endswith($MSG, baz_list, ignorecase=true))
+    {
+        result.endswith_baz = true;
+    };
+    $MSG = json();
+    $MSG = result;
+    """, msg="fooBARbAz",
+    )
+    syslog_ng.start(config)
+
+    assert "processed" not in file_false.get_stats()
+    assert file_true.read_log() == '{"startswith_foo":true,"contains_bar":true,"endswith_baz":true}\n'
